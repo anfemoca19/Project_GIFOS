c080c99f26e3efc146365856c35c676a0a8de362
Andres  Felipe Morales <anfemoca19@github.com>
2021-03-08 18:35:11 -0500

    Fixed code variables

--- Js/Js.js
+++ Js/Js.js
@@ -1,10 +1,10 @@
 //Menu hamburgue
 addEventListener('DOMContentLoaded', ()=> {
     const btn_mobile = document.querySelector('.btn-mobile')
-    if(btn_menu){
-        btn_menu.addEventListener('click', ()=>{
+    if (btn_mobile) {
+        btn_mobile.addEventListener('click', ()=>{
             const menu_list_content = document.querySelector('.menu-list-content')
-            menu-list-content.classList.toggle('show')        
+            menu_list_content.classList.toggle('show')        
         });
     }
 });
