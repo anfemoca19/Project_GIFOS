8fbcfdeaae2ca064d1937fa49cb9eadd062fe867
Andres  Felipe Morales <anfemoca19@github.com>
2021-05-15 17:43:20 -0500

    Subo  cambios

--- Js/Js.js
+++ Js/Js.js
@@ -1,10 +1,29 @@
 //Menu hamburgue
 addEventListener('DOMContentLoaded', ()=> {
     const btn_mobile = document.querySelector('.btn-mobile')
+    const x = document.querySelector('.x')
     if (btn_mobile) {
         btn_mobile.addEventListener('click', ()=>{
-            const menu_list_content = document.querySelector('.menu-list-content')
-            menu_list_content.classList.toggle('show')        
+            const menu_list_content = document.querySelector('.menu-list')
+            menu_list_content.classList.toggle('show')
+            if (menu_list_content.classList.contains('show')) {
+                x.classList.add('show-x')
+                btn_mobile.classList.add('hide-btn')
+            }    
         });
     }
+
+    if (x) {
+        x.addEventListener('click', ()=>{
+            const menu_list_content = document.querySelector('.menu-list')
+            menu_list_content.classList.toggle('show')
+            if (x.classList.contains('show-x')) {
+                x.classList.remove('show-x')
+                btn_mobile.classList.remove('hide-btn')
+            }    
+        });
+    }
+   
+
+    
 });
